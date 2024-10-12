a8adfebdd1e9c549269c4fee8c2e4461a8f0e3e3
王政 <everywangbug@gmail.com>
2024-10-12 17:50:55 +0800

    playground import url 使用 blob url替代，直接在控制台运行

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { transform } from "@babel/standalone"
+import { useRef } from "react"
+
 export const App: React.FC = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  const onClick = () => {
+    if (!textAreaRef.current) {
+      return
+    }
+
+    const res = transform(textAreaRef.current.value, {
+      presets: ["react", "typescript"],
+      filename: "index.tsx",
+    })
+    console.log(res.code, 'placeholder')
+  }
+
+  const code = `import { useEffect, useState } from "react";
+    const App = () => {
+      const [count, setCount] = useState(0);
+      useEffect(() => {
+        setCount(1);
+      }, []);
+      return <div onClick={() => setCount(preNum => preNum + 1)}>{count}</div>;
+    };
+
+    export default App;
+  `
+  
   return (
-    <div>App</div>
+    <div>
+      <textarea ref={textAreaRef} style={{ width: '500px', height: '300px'}} defaultValue={code}></textarea>
+      <button onClick={onClick}>编译</button>
+    </div>
   )
 }
