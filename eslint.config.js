a19cc0b7dc2d9ea165cd9e7c47cd9cc733e8afdb
王政 <everywangbug@gmail.com>
2024-11-18 17:59:51 +0800

    eslint 格式化

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,19 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      // 双引号
+      'quotes': ["error", "single"],
+      // 分号结尾语句
+      'semi': ["error", "always"],
+      // 禁止使用 console
+      'no-console': 'warn',
+      // 禁止使用 debugger
+      'no-debugger': 'warn',
+      // 禁止使用 var
+      'no-var': 'error',
+      // 禁止使用未声明的变量
+      'no-undeclared': 'error',
+      // 禁止使用未定义的变量"]
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
