a650f9763ebbf7bc875b1c0cf66c150e76239d1b
王政 <everywangbug@gmail.com>
2024-11-29 17:23:10 +0800

    提取文件后缀

--- src/preview/compiler.ts
+++ src/preview/compiler.ts
@@ -17,7 +17,40 @@ export const babelTransform = (filename: string, code: string, files: Files) => 
   return res
 }
 
+export const getModuleFile = (files: Files, modulePath: string) => {
+  let moduleName = modulePath.split('/').pop() || ''
+  if (!moduleName.includes('.')) {
+     const realModuleName = Object.keys(files).filter(item => {
+       return item.endsWith('.tsx')
+         || item.endsWith('.ts')
+         || item.endsWith('.jsx')
+         || item.endsWith('.js')
+     }).find(item => {
+        return item.split('.').includes(moduleName)
+     })
+     if (realModuleName) {
+      moduleName = realModuleName
+     }
+     return files[moduleName]
+  }
+}
+
 export const compile = (files: Files) => {
   const main = files[ENTRY_FILE_NAME]
   return babelTransform(ENTRY_FILE_NAME, main.value, files)
+}
+
+export const customResolver = (files: Files) => {
+  return {
+    visitor: {
+      ImportDeclaration(path) {
+        // path.node.source.value = files[path.node.source.value].value
+        path.node.source.value = '123456'
+      }
+    }
+  }
+}
+
+const josnToJs = (file: File) => {
+
 }
