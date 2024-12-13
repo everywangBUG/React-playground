5b024c8ad491b55678e1cc7133cdd3ec2dfb8fcf
王政 <everywangbug@gmail.com>
2024-12-13 11:16:22 +0800

    feat: copy link to share

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ import { useContext } from "react"
 import logo from "../assets/logo.svg"
 import { PlaygroundContext } from "../context/PlaygroundContext"
 import "./Header.scss"
-import { MoonOutlined, SunOutlined } from "@ant-design/icons"
+import { MoonOutlined, ShareAltOutlined, SunOutlined } from "@ant-design/icons"
+import copy from "copy-to-clipboard"
+import { message } from "antd"
 
 export const Header: React.FC = () => {
   const { theme, setTheme } = useContext(PlaygroundContext)
@@ -28,6 +30,13 @@ export const Header: React.FC = () => {
             onClick={() => setTheme("light")} 
           />
         }
+        <ShareAltOutlined
+          style={{marginLeft: "10px"}}
+          onClick={() => {
+            copy(window.location.href)
+            message.success("已复制链接")
+          }}
+        />
       </div>
     </div>
   )
--- src/context/PlaygroundProvider.tsx
+++ src/context/PlaygroundProvider.tsx
@@ -1,13 +1,26 @@
-import { PropsWithChildren, useState } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import { Files, PlaygroundContext, Theme } from "./PlaygroundContext"
 import { initFiles } from "../playground/data"
 import { fileNameToLanguage } from "../utils/fileNameToLanguage"
+import { compress, uncompress } from "../utils/utils"
+
+const getFileFromUrl = () => {
+  let files: Files | undefined
+  try {
+    const hash = uncompress(window.location.hash.slice(1))
+    files = JSON.parse(hash)
+  } catch (error) {
+    console.log(error)
+  }
+  return files
+}
 
 export const PlaygroundProvider: React.FC<PropsWithChildren> = (props) => {
   const { children } = props
-  const [files, setFiles] = useState<Files>(initFiles)
+  const [files, setFiles] = useState<Files>(getFileFromUrl() || initFiles)
   const [selectedFileName, setSelectedFileName] = useState("App.tsx")
   const [theme, setTheme] = useState<Theme>("dark")
+
 
   const addFile = (fileName: string) => {
     setFiles({
@@ -40,6 +53,11 @@ export const PlaygroundProvider: React.FC<PropsWithChildren> = (props) => {
     delete files[fileName]
     setFiles({...files})
   }
+
+  useEffect(() => {
+    const hash = compress(JSON.stringify(files))
+    window.location.hash = hash
+  }, [files])
 
   return (
     <PlaygroundContext.Provider
